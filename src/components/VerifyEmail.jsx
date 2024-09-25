3571f41cc5c49a2cd1406790a5de328e27745a1e
VictorNorm <VictorNorm@users.noreply.github.com>
2024-09-25 13:49:07 +0200

    updated verify-email code

--- src/components/VerifyEmail.jsx
+++ src/components/VerifyEmail.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const VerifyEmail = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const query = new URLSearchParams(location.search);
   const token = query.get('token');
   const [message, setMessage] = useState('Verifying your email...');
 
   useEffect(() => {
     const verifyEmail = async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/verify-email?token=${token}`);
-      const result = await response.text(); // since we are using res.send() for plain text
-      setMessage(result);
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/verify-email?token=${token}`);
+        const result = await response.json();
+        setMessage(result.message);
+        
+        // Redirect to login page after a short delay
+        setTimeout(() => {
+          navigate('/login');
+        }, 2500);
+      } catch (error) {
+        setMessage('An error occurred while verifying your email.');
+      }
     };
 
     if (token) {
       verifyEmail();
     }
-  }, [token]);
+  }, [token, navigate]);
 
   return (
     <div>
